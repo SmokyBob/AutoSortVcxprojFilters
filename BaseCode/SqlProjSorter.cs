325ba808cccd28a7affb1409d044a3e8529fbc91
Mauro Solcia <mauro.solcia@gmail.com>
2021-11-20 09:15:40 +0100

    Something broke and I don't seem to be able to get back to the previous version...
    Going to start with a fresh vsix project and add one thing at the time while learining how shared projects works

--- BaseCode/SqlProjSorter.cs
+++ BaseCode/SqlProjSorter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
-namespace BaseCode
+namespace AutoSortSqlProj
 {
     class SqlProjSorter
     {
---
+++ VS2019/MenuItemCommand.cs
@@ -0,0 +1,125 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+using System.Linq;
+
+namespace VS2019
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class MenuItemCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("b180da1d-47cd-4261-bb69-1452e784b9d6");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private EnvDTE.DTE m_dte;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuItemCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private MenuItemCommand(AsyncPackage package, OleMenuCommandService commandService, EnvDTE.DTE dte)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+            m_dte = dte;
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static MenuItemCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in MenuItemCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            EnvDTE.DTE dte = await package.GetServiceAsync(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+            Instance = new MenuItemCommand(package, commandService,dte);
+        }
+
+        
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            //ThreadHelper.ThrowIfNotOnUIThread();
+            //string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            //string title = "MenuItemCommand";
+
+            //// Show a message box to prove we were here
+            //VsShellUtilities.ShowMessageBox(
+            //    this.package,
+            //    message,
+            //    title,
+            //    OLEMSGICON.OLEMSGICON_INFO,
+            //    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            //    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+            //m_dte.Solution.Projects
+            var projects = m_dte.Solution.Projects
+                .Cast<EnvDTE.Project>()
+                .Where(x => { return x?.Object != null; })
+                .ToArray(); ;
+
+            foreach (var proj in projects)
+            {
+                //... Should exclude non .sqlproj ... but the sort function already does that...
+                BaseCode.SqlProjSorter.Sort(proj.FullName);
+            }
+        }
+    }
+}
